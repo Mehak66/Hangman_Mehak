779f65fc8cf1e0a9210efdccde7b9a5009e504f8
mehak66 <randhawamehak41@gmail.com>
2020-12-15 22:38:27 +0530

    Add project files.

---
+++ Hangman_Mehak/Resources/Score_Table.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hangman_Mehak.Resources
+{
+    public class Score_Table
+    {
+        [PrimaryKey, AutoIncrement] //Column("_Id")]
+        public int Id { get; set; }
+
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+    }
+}
